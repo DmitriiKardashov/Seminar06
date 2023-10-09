e6823da7581b61b7da24fb9455a643271919ddd5
DimiyanKo <karddmitrij@yandex.ru>
2023-10-09 21:33:42 +0300

    Task44

---
+++ Task44/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] FiboArreay(int num)
+{
+    int[] fibonacci = new int[num];
+    fibonacci[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    return fibonacci;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <2) 
+{
+    Console.WriteLine("Введите число большее или равноее 2.");
+    return;
+}
+
+int[] res = FiboArreay(number);
+PrintArray(res);
