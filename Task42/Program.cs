fae0b755c1c8784ecc68cada8f4380f31693bd94
DimiyanKo <karddmitrij@yandex.ru>
2023-10-09 17:56:02 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите основание системы счисления:");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+int DecToBaseNum(int num, int baseNum) //Универсальный метод перевода в разные единицы счисления
+{
+    int result = 0;
+    int mult = 1;
+    while (num > 0)
+    {
+        result += num%baseNum * mult;
+        num /= baseNum;
+        mult *= 10;
+    }
+    return result;
+}
+
+int res = DecToBaseNum(number, baseNumber);
+Console.WriteLine($"{number} в {baseNumber}ой системе счисления -> {res}");
