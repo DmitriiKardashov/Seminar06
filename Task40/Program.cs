a9128ab420e5c524778c77db3a90da20da78a4ee
DimiyanKo <karddmitrij@yandex.ru>
2023-10-09 17:16:59 +0300

    Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите первую сторону:");
+int side1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую сторону:");
+int side2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третью сторону:");
+int side3 = Convert.ToInt32(Console.ReadLine());
+
+bool IsExistTriangle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+bool result = IsExistTriangle(side1, side2, side3);
+Console.WriteLine(result ? "Треугольник существует" : "Треугольник не существует");
